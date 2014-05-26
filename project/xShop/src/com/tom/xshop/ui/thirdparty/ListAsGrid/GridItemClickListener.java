4ee7ea12ef9f108c0e7d52547b14587a52330a1a
leozhangchao <leo_zhangchao@hotmail.com>
2014-05-27 00:05:09 +0800

    collect some third party good widget
    1. touch image
    2. badge view
    3. list as grid view

---
+++ project/xShop/src/com/tom/xshop/ui/thirdparty/ListAsGrid/GridItemClickListener.java
@@ -0,0 +1,9 @@
+package com.tom.xshop.ui.thirdparty.ListAsGrid;
+
+import android.view.View;
+
+public interface GridItemClickListener {
+
+	void onGridItemClicked(View v, int position, long itemId);
+
+}
